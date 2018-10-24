81f73aeca4dcc18c119da12f7b415552c874ef49
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 20:45:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(input, board)
-  input.between?(0, 8) && (position_taken?(board, number_entered)
+  input.between?(0, 8) && !(position_taken?(board, number_entered)
 end
 
 def position_taken?(board, index)
