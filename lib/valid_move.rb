c3417de168998204630a21c39b454cdbd15f25a7
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 20:39:02 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,28 @@
+def position_taken?(board,position)
+  if board[position] == " "
+    return false
+    elsif board[position] == ""
+      return false
+    elsif board[position] == nil
+      return false
+    else board[position] == "X" || "O"
+      return true
+  end
+end
 
+def valid_move(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+      return false
+    elsif board[index] == nil
+      return false
+    else board[index] == "X" || "O"
+      return true
+  end
+end
+
+end
 # code your #valid_move? method here
 
 
