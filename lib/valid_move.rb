8ee6816fb8f7b1ecf0a0c760a2a6410cad98cdd9
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 20:50:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,14 +1,3 @@
-def position_taken?(board,position)
-  if board[position] == " "
-    return false
-    elsif board[position] == ""
-      return false
-    elsif board[position] == nil
-      return false
-    else board[position] == "X" || "O"
-      return true
-  end
-end
 
 def valid_move?(position, board)
   if position.between?(0, 8) && !(position_taken?(board, position)
