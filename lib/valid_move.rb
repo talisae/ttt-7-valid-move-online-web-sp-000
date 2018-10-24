6af522748ed1813efe1ff1c1e9c2216c19faa752
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 20:49:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,24 @@
+def position_taken?(board,position)
+  if board[position] == " "
+    return false
+    elsif board[position] == ""
+      return false
+    elsif board[position] == nil
+      return false
+    else board[position] == "X" || "O"
+      return true
+  end
+end
+
+
 def valid_move?(input, board)
-  input.between?(0, 8) && !(position_taken?(board, number_entered)
+  if input.between?(0, 8) && !(position_taken?(board, number_entered))
 end
 
 def position_taken?(board, index)
  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
+
 # code your #valid_move? method here
 
 
